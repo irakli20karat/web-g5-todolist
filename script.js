bb3a993edcaa75ae0e321ef237eaad6f6f80da6a
03Heraklion <irakli20karat>
2026-02-24 20:19:13 +0400

    added date

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.getElementById("todo-input");
+const todoInputDate = document.getElementById("todo-input-date");
 const todoList = document.getElementById("todo-list");
 
-const todos = JSON.parse(localStorage.getItem("todos")) || [];
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 const saveToLocalStorage = () => {
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -11,31 +12,32 @@ const saveToLocalStorage = () => {
 const renderTodos = () => {
     todoList.innerHTML = "";
     todos.forEach((todo) => {
+        let date = new Date(todo.date).toDateString();
         const li = document.createElement("li");
-        li.className = `todo-item ${todo.completed ? "completed" : ""}`;
-        li.innerHTML = `<span>${todo.text}</span>
-            <div class="actions">
-                <button class="btn-check" onclick="toggleTodo(${todo.id})">done</button>
-                <button class="btn-delete" onclick="deleteTodo(${todo.id})">remove</button>
-            </div>
-        `;
+        li.className = `todo-item ${todo.completed ? 'completed' : ''}`;
+        li.innerHTML = `<span><p class="date">${date}</p><p>${todo.text}</p></span>
+        <div class="actions">
+            <button class="btn-check" onclick="doneBtnClickHandler(${todo.id})">done</button>
+            <button class="btn-delete" onclick="deleteBtnClickHandler(${todo.id})">remove</button>
+        </div>
+`;
         todoList.appendChild(li);
     });
 }
 
-const toggleTodo = (id) => {
-    const todo = todos.map((o) =>
-        o.id === id ? { ...o, completed: !o.completed } : o
-    );
-    todos.splice(0, todos.length, ...todo);
-
+const deleteBtnClickHandler = (id) => {
+    todos = todos.filter((todo) => todo.id !== id)
     saveToLocalStorage();
     renderTodos();
 }
 
-const deleteTodo = (id) => {
-    const index = todos.filter((o) => o.id === id)[0];
-    todos.splice(todos.indexOf(index), 1);
+const doneBtnClickHandler = (id) => {
+    todos = todos.map((todo) => {
+        if (todo.id === id) {
+            return { ...todo, completed: !todo.completed };
+        }
+        return todo
+    })
 
     saveToLocalStorage();
     renderTodos();
@@ -45,17 +47,19 @@ todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const inputValue = todoInput.value.trim();
-    if (inputValue === "") return;
-    console.log(inputValue);
+    todoInput.value = '';
+    if (!inputValue) return;
+
+    const dateValue = todoInputDate.value;
+    todoInputDate.value = '';
+    if (!dateValue) return;
 
     const newTodo = {
         id: Date.now(),
         text: inputValue,
+        date: dateValue,
         completed: false
     }
-
-    todoInput.value = "";
-
     todos.push(newTodo);
     console.log(todos);
     saveToLocalStorage();
